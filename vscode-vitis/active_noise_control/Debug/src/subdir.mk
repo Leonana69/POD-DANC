95337c07d1619e3d67ef45800f630093a9a4ab7b
leonana69 <chengj0803@gmail.com>
2023-04-13 20:49:04 -0400

    update make file

--- vscode-vitis/active_noise_control/Debug/src/subdir.mk
+++ vscode-vitis/active_noise_control/Debug/src/subdir.mk
@@ -22,8 +22,5 @@ C_DEPS = ./src/*.d
 -include ../../../user.mk
 PLATFORM_INCLUDE_PATH := ../../$(PLATFORM_NAME)/export/$(PLATFORM_NAME)/sw/$(PLATFORM_NAME)/standalone_ps7_cortexa9_0/bspinclude/include
 src/%.o: ../src/%.c
-	@echo 'Building file: $<'
-	@echo 'Invoking: ARM v7 gcc compiler'
-	arm-none-eabi-gcc -Wall -O0 -g3 -c -fmessage-length=0 -MT"$@" -mcpu=cortex-a9 -mfpu=vfpv3 -mfloat-abi=hard -I$(PLATFORM_INCLUDE_PATH) -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
-	@echo 'Finished building: $<'
-	@echo ' '
+	@echo "  CC    $@"
+	@arm-none-eabi-gcc -Wall -O0 -g3 -c -fmessage-length=0 -MT"$@" -mcpu=cortex-a9 -mfpu=vfpv3 -mfloat-abi=hard -I$(PLATFORM_INCLUDE_PATH) -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
