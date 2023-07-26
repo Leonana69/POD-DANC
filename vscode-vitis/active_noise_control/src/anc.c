4f7377e7c85c2997d04201631c8f6b531e9acb70
leonana69 <chengj0803@gmail.com>
2023-07-26 17:44:19 -0400

    add chirp generation

--- vscode-vitis/active_noise_control/src/anc.c
+++ vscode-vitis/active_noise_control/src/anc.c
@@ -12,47 +12,82 @@
 // NEW_FIR(main_fir, 32);
 // NEW_FIR(second_fir, 128);
 
-void calibration() {
-    // chirp generation
-    int len = 16000;
-    int fs = 16000;
-    int f1 = 25; // start freq
-    int f2 = 2000; // end freq
-    
-    float dt = 1.0f / fs;
-    float dur = len * dt;
+struct Calibration {
+    int len;
+    int fs;
+    int f1;
+    int f2;
+    float dt;
+    float dur;
+    float ff;
+    float p1;
+    float p2;
+};
 
-    float ff = logf(f2 / f1);
-    float p1 = 2 * 3.141592653575 * f1 * dur / ff;
-    float p2 = ff / len;
-
-    int32_t *ptr = (int32_t *)PLAY_BUFFER_0;
-    for (int i = 0; i < len; i++) {
-        // int32_t data = (32768.0f * sinf(p1 * (expf(p2 * i) - 1))) * 128.0f;
-        int32_t data = (32768.0f * sinf(2*3.141592635*1600*i*dt)) * 128.0f;
-        ptr[i * 2] = data;
-        ptr[i * 2 + 1] = -data;
-    }
-
-    Xil_DCacheFlushRange((u32) PLAY_BUFFER_0, 2 * DATA_BYTE_LENGTH * len);
-    // Xil_DCacheInvalidateRange((u32) MEM_BASE_ADDR, 2 * DATA_BYTE_LENGTH * NR_AUDIO_SAMPLES);
-    // fnAudioPlay(sAxiDma, len);
-    // wait until play end
-    // while (!ANC_INSTANCE.fDmaMM2SEvent);
+void calibration_init(int f1, int f2, int len, int fs, struct Calibration *calibration) {
+    calibration->len = len;
+    calibration->fs = fs;
+    calibration->f1 = f1;
+    calibration->f2 = f2;
+    calibration->dt = 1.0f / fs;
+    calibration->dur = len * calibration->dt;
+    calibration->ff = logf(f2 / f1);
+    calibration->p1 = 2 * 3.141592653575 * f1 * calibration->dur / calibration->ff;
+    calibration->p2 = calibration->ff / len;
 }
 
+int32_t calibration_get_data(struct Calibration *calibration, int i) {
+    return (int32_t)((32768.0f * sinf(calibration->p1 * (expf(calibration->p2 * i) - 1))) * 128.0f);
+}
+
+struct Calibration calibration;
 
 void run() {
     xil_printf("====> ANC start <====\n\r");
 
-    calibration();
+    // init chirp generation
+    calibration_init(25, 2000, 16000, 16000, &calibration);
 
     fnSetLineInput();
     fnNonStopInit();
 
     int Status;
     int lastBufferIndex = 0;
-    
+
+    // generate chirp
+    for (int i = 0; i < 16000 / BUFFER_SAMPLES; i++) {
+        // start record for BUFFER_SAMPLE samples
+        Status = XAxiDma_SimpleTransfer(&sAxiDma0, (UINTPTR) GET_RECORD_BUFFER(lastBufferIndex), BUFFER_SIZE, XAXIDMA_DEVICE_TO_DMA);
+        if (Status != XST_SUCCESS) {
+            xil_printf("@ record start [FAILED]\n\r");
+        }
+        // start play for BUFFER_SAMPLE samples
+        
+        Status = XAxiDma_SimpleTransfer(&sAxiDma1, (UINTPTR) GET_PLAY_BUFFER(lastBufferIndex), BUFFER_SIZE, XAXIDMA_DMA_TO_DEVICE);
+        if (Status != XST_SUCCESS) {
+            xil_printf("@ playback start [FAILED]\n\r");
+        }
+
+        // process previous buffer
+        lastBufferIndex = 1 - lastBufferIndex;
+        u32 addrRecord = GET_RECORD_BUFFER(lastBufferIndex);
+        u32 addrPlay = GET_PLAY_BUFFER(lastBufferIndex);
+
+        Xil_DCacheInvalidateRange(addrRecord, BUFFER_SIZE);
+        for (int j = 0; j < BUFFER_SAMPLES; j++) {
+            int32_t data = calibration_get_data(&calibration, i * BUFFER_SAMPLES + j);
+            ((int32_t *)addrPlay)[2 * j] = data;
+            ((int32_t *)addrPlay)[2 * j + 1] = data;
+        }
+        Xil_DCacheFlushRange(addrPlay, BUFFER_SIZE);
+
+        // wait until DMA transmit ends
+        while (XAxiDma_Busy(&sAxiDma0, XAXIDMA_DEVICE_TO_DMA)) {}
+        while (XAxiDma_Busy(&sAxiDma1, XAXIDMA_DMA_TO_DEVICE)) {}
+    }
+
+    xil_printf("====> calibration done <====\n\r");
+    lastBufferIndex = 0;
     while (1) {
         // start record for BUFFER_SAMPLE samples
         Status = XAxiDma_SimpleTransfer(&sAxiDma0, (UINTPTR) GET_RECORD_BUFFER(lastBufferIndex), BUFFER_SIZE, XAXIDMA_DEVICE_TO_DMA);
